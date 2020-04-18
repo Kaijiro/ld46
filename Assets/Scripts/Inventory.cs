f1d39e0ab00db36978d265c213aaa02768187cde
OkkoLOBR <olivier.kowalczyk@gmail.com>
2020-04-18 22:41:37 +0200

    Add Inventory ugly UI, and add

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -6,6 +6,8 @@ public class Inventory : MonoBehaviour
 {
 
     [SerializeField] private InventoryItem[] playerInventory;
+    public Transform InventoryUI;
+    public Sprite[] itemSprites; 
     public int maxQty = 6;
     private int currentQty = 0;
 
@@ -13,20 +15,14 @@ public class Inventory : MonoBehaviour
     {
         if (currentQty < maxQty)
         {
-
             foreach (InventoryItem tmp in playerInventory)
             {
-                if (tmp.name == newItem)
-                {
-                    tmp.quantity = tmp.quantity + 1;
-                    currentQty += 1;
-                    break;
-                }
-                if (tmp.name == "free")
+                if (tmp.name == newItem || tmp.name == "free")
                 {
                     tmp.quantity = tmp.quantity + 1;
                     tmp.name = newItem;
-                    currentQty += 1;
+                    UpdateInventoryUI(newItem, "add");
+                    currentQty += 1;                    
                     break;
                 }
             }
@@ -36,20 +32,58 @@ public class Inventory : MonoBehaviour
         }
     }
 
+    public void UpdateInventoryUI( string itemName, string mode)
+    {
+        int itemSpriteIdx = 0;
+        // WARNING : UGLY DIRTY CODE
+        switch(itemName)
+        {
+            case "can":
+                itemSpriteIdx = 0;
+                break;
+            case "herb":
+                itemSpriteIdx = 1;
+                break;
+            case "milk":
+                itemSpriteIdx = 2;
+                break;
+            case "chicken":
+                itemSpriteIdx = 3;
+                break;
+            case "salmon":
+                itemSpriteIdx = 4;
+                break;
+        }
+
+        if ( mode == "add" )
+        {
+            InventoryUI.GetChild(currentQty).GetComponent<SpriteRenderer>().sprite = itemSprites[itemSpriteIdx];
+        }
+
+        if (mode == "remove")
+        {
+            // InventoryUI.GetChild(currentQty).GetComponent<SpriteRenderer>().sprite = null;
+        }
+
+    }
+
     public void RemoveItem(string newItem)
     {
         foreach (InventoryItem tmp in playerInventory)
         {
-            if (tmp.name == newItem)
+            if (tmp.name == newItem && tmp.quantity > 0)
             {
-                if (tmp.quantity > 0)
-                {
-                    tmp.quantity = tmp.quantity - 1;
-                    currentQty -= 1;
-                    break;
-                }                
+                tmp.quantity = tmp.quantity - 1;
+                currentQty -= 1;
+                UpdateInventoryUI(newItem, "remove");
+                break;            
             }            
         }
+    }
+
+    public bool IsInventoryFull()
+    {
+        return currentQty >= maxQty;
     }
 
     public int GetItemQty(string newItem)
