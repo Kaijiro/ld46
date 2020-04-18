fa20e513c486ac0fc1334fe31d61fc573aaaf273
OkkoLOBR <olivier.kowalczyk@gmail.com>
2020-04-18 16:40:44 +0200

    No more rocket jump

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,22 +1,18 @@
-﻿// This script handles inputs for the player. It serves two main purposes: 1) wrap up
-// inputs so swapping between mobile and standalone is simpler and 2) keeping inputs
-// from Update() in sync with FixedUpdate()
-
+﻿
 using UnityEngine;
 
 [DefaultExecutionOrder(-100)]
 public class PlayerInput : MonoBehaviour
 {
 
-	[HideInInspector] public float horizontal;		//Float that stores horizontal input
-	[HideInInspector] public bool jumpPressed;			//Bool that stores jump pressed
-	bool readyToClear;								//Bool used to keep input in sync
+	[HideInInspector] public float horizontal;		
+	[HideInInspector] public bool jumpPressed;		
+	bool readyToClear;								
 
 
 
 	void Update()
 	{
-		//Clear out existing input values
 		ClearInput();
 
 		//If the Game Manager says the game is over, exit
@@ -25,27 +21,22 @@ public class PlayerInput : MonoBehaviour
 			return;
 		*/
 
-		//Process keyboard, mouse, gamepad (etc) inputs
+		
 		ProcessInputs();
-
-		//Clamp the horizontal input to be between -1 and 1
+				
 		horizontal = Mathf.Clamp(horizontal, -1f, 1f);
 	}
 
 	void FixedUpdate()
 	{
-		//In FixedUpdate() we set a flag that lets inputs to be cleared out during the 
-		//next Update(). This ensures that all code gets to use the current inputs
 		readyToClear = true;
 	}
 
 	void ClearInput()
 	{
-		//If we're not ready to clear input, exit
 		if (!readyToClear)
 			return;
-
-		//Reset all inputs
+		
 		horizontal		= 0f;
 		jumpPressed		= false;
 		readyToClear	= false;
@@ -53,17 +44,8 @@ public class PlayerInput : MonoBehaviour
 
 	void ProcessInputs()
 	{
-		//Accumulate horizontal axis input
 		horizontal		+= Input.GetAxis("Horizontal");
-
-		//Accumulate button inputs
 		jumpPressed		= jumpPressed || Input.GetKeyDown("space");
-
-		if (Input.GetKeyDown("space"))
-        {
-			Debug.Log("space pressed");
-			Debug.Log(jumpPressed);
-        }
 	}
 
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,127 +1,81 @@
-﻿// This script controls the player's movement and physics within the game
-
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public bool drawDebugRaycasts = true;	//Should the environment checks be visualized
+	public float speed = 8f;				
+	public float maxFallSpeed = -25f;       
+	public float footOffset = .4f;          
+	public float groundDistance = .87f;     
+	public LayerMask groundLayer;           
 
-	[Header("Movement Properties")]
-	public float speed = 8f;				//Player speed
-	public float coyoteDuration = .05f;		//How long the player can jump after falling
-	public float maxFallSpeed = -25f;       //Max speed player can fall
+	public float jumpForce = 6.3f;          
+	public bool isJumping;					
 
+	PlayerInput input;						
+	BoxCollider2D bodyCollider;				
+	Rigidbody2D rigidBody;					
 
-	[Header("Jump Properties")]
-	public float jumpForce = 6.3f;          //Initial force of jump
-	public float jumpHoldDuration = .1f;    //How long the jump key can be held
-
-	[Header ("Status Flags")]
-	public bool isJumping;					//Is player jumping?
-
-	PlayerInput input;						//The current inputs for the player
-	BoxCollider2D bodyCollider;				//The collider component
-	Rigidbody2D rigidBody;					//The rigidbody component
-	
-	float jumpTime;							//Variable to hold jump duration
-	float coyoteTime;						//Variable to hold coyote duration
-	float playerHeight;						//Height of the player
-
-	float originalXScale;					//Original scale on X axis
-	int direction = 1;						//Direction player is facing
-
-	Vector2 colliderStandSize;				//Size of the standing collider
-	Vector2 colliderStandOffset;			//Offset of the standing collider
-
-	const float smallAmount = .05f;			//A small amount used for hanging position
+	float originalXScale;					
+	int direction = 1;						
 
 
 	void Start ()
 	{
-		//Get a reference to the required components
 		input = GetComponent<PlayerInput>();
 		rigidBody = GetComponent<Rigidbody2D>();
 		bodyCollider = GetComponent<BoxCollider2D>();
-
-		//Record the original x scale of the player
 		originalXScale = transform.localScale.x;
-
-		//Record the player's height from the collider
-		playerHeight = bodyCollider.size.y;
-
-		//Record initial collider size and offset
-		colliderStandSize = bodyCollider.size;
-		colliderStandOffset = bodyCollider.offset;
 	}
 
 	void FixedUpdate()
 	{
-
-		//Process ground and air movements
+		PhysicsCheck();
 		GroundMovement();		
 		MidAirMovement();
 	}
 
-	void GroundMovement()
+	void PhysicsCheck()
+	{
+		Vector2 pos = transform.position;
+		Vector2 lOffset = new Vector2(-footOffset, 0f);
+		Vector2 rOffset = new Vector2(footOffset, 0f);
+		RaycastHit2D leftCheck = Physics2D.Raycast(pos + lOffset, Vector2.down, groundDistance, groundLayer);
+		RaycastHit2D rightCheck = Physics2D.Raycast(pos + rOffset, Vector2.down, groundDistance, groundLayer);
+
+		if (leftCheck || rightCheck)
+			isJumping = false;
+	}
+
+		void GroundMovement()
 	{
 
-		//Calculate the desired velocity based on inputs
 		float xVelocity = speed * input.horizontal;
-
-		//If the sign of the velocity and direction don't match, flip the character
 		if (xVelocity * direction < 0f)
 			FlipCharacterDirection();
 
-		//Apply the desired velocity 
 		rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
 	}
 
 	void MidAirMovement()
 	{
-
-		//If the jump key is pressed AND the player isn't already jumping AND EITHER
-		//the player is on the ground or within the coyote time window...
 		if (input.jumpPressed && !isJumping)
 		{
-
-			//...The player is no longer on the groud and is jumping...
 			isJumping = true;
-
-			//...record the time the player will stop being able to boost their jump...
-			jumpTime = Time.time + jumpHoldDuration;
-
-			//...add the jump force to the rigidbody...
 			rigidBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-
-			//...and tell the Audio Manager to play the jump audio
 			// AudioManager.PlayJumpAudio();
 		}
-		//Otherwise, if currently within the jump time window...
-		else if (isJumping)
-		{
-			//...and if jump time is past, set isJumping to false
-			if (jumpTime <= Time.time)
-				isJumping = false;
-		}
 
-		//If player is falling to fast, reduce the Y velocity to the max
 		if (rigidBody.velocity.y < maxFallSpeed)
 			rigidBody.velocity = new Vector2(rigidBody.velocity.x, maxFallSpeed);
 	}
 
 	void FlipCharacterDirection()
 	{
-		//Turn the character by flipping the direction
 		direction *= -1;
-
-		//Record the current scale
 		Vector3 scale = transform.localScale;
-
-		//Set the X scale to be the original times the direction
 		scale.x = originalXScale * direction;
-
-		//Apply the new scale
 		transform.localScale = scale;
 	}
+
 
 }
