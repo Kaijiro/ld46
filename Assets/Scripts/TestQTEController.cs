2c123e662323d80c9938adae843c39ec46f322b2
Tristan Lasalle <tristan.lasalle@gmail.com>
2020-04-18 22:14:18 +0200

    Display tooltip by level

--- Assets/Scripts/TestQTEController.cs
+++ Assets/Scripts/TestQTEController.cs
@@ -13,7 +13,10 @@ public class TestQTEController : MonoBehaviour
 
     private Text _descriptionField;
     public GameObject recipeDescriptionText;
-
+    public GameObject level1Helps;
+    public GameObject level2Helps;
+    public GameObject level3Helps;
+    
     private int _playerLevel = 1;
 
     private void Awake()
@@ -37,6 +40,11 @@ public class TestQTEController : MonoBehaviour
 
     private void UpdateActionListPanel()
     {
-        
+        switch (_playerLevel)
+        {
+            case 1: level1Helps.SetActive(true); break;
+            case 2: level2Helps.SetActive(true); break;
+            case 3: level3Helps.SetActive(true); break;
+        }
     }
 }
